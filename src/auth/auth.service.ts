7c92a3ac30a1095044b8e037fdf25099c6d4bacb
Edivan <edimasepa@gmail.com>
2025-10-08 21:22:04 -0300

    feat (auth): configuração e função getToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
+
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ){}
+
+  async getToken(payload: {sub: number; login: string}){
+    
+    const acessToken = await this.jwtService.signAsync(
+      {sub: payload.sub, login: payload.login},
+      {
+        secret: this.configService.get<string>('JWT_SECRET'),
+        expiresIn: this.configService.get<string>('JWT_EXPIRES_IN')
+      }
+    )
+    
+    const refreshToken = await this.jwtService.signAsync(
+      {sub: payload.sub, login: payload.login},
+      {
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+        expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRES_IN'),
+      }
+    )
+    return {acessToken, refreshToken};
+  }
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
