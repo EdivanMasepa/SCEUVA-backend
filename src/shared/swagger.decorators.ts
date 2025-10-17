da9fad4e96e9fd4d523830ea91bad75fbbb269e7
Edivan <edimasepa@gmail.com>
2025-10-16 21:47:29 -0300

    feat: função de tipagem de retornos no swagger

---
+++ src/shared/swagger.decorators.ts
@@ -0,0 +1,28 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse, ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiUnauthorizedResponse, ApiForbiddenResponse, ApiNotFoundResponse } from '@nestjs/swagger';
+
+interface ApiResponseItem {
+  status: number;
+  description: string;
+}
+
+export function ApiResponses(responses: ApiResponseItem[]) {
+  const decorators = responses.map(({ status, description }) => {
+    switch (status) {
+      case 400:
+        return ApiBadRequestResponse({ description });
+      case 401:
+        return ApiUnauthorizedResponse({ description });
+      case 403:
+        return ApiForbiddenResponse({ description });
+      case 404:
+        return ApiNotFoundResponse({ description });
+      case 500:
+        return ApiInternalServerErrorResponse({ description });
+      default:
+        return ApiResponse({ status, description });
+    }
+  });
+
+  return applyDecorators(...decorators);
+}
