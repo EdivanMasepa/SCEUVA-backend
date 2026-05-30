83c00f539c687e26b6828ff400855539d38ef05b
Edivan <edimasepa@gmail.com>
2026-05-29 22:02:41 -0300

    fix (user): parameter of swagger in dto

--- src/modules/user/dto/change-password.dto.ts
+++ src/modules/user/dto/change-password.dto.ts
@@ -1,21 +1,26 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class ChangePasswordDTO {
 
+    @ApiPropertyOptional()
     @IsNumber({allowNaN: false}, {message: 'CODIGO deve ser um número.'})
     @IsInt({message: 'CODIGO deve ser um número inteiro.'})
     @IsOptional()
     code?: number;
 
+    @ApiProperty()
     @IsNotEmpty({message: 'SENHA ATUAL é obrigatória.'})
     @IsString({message: 'SENHA ATUAL deve ser do tipo texto.'})
     currentPassword: string;
 
+    @ApiProperty()
     @IsNotEmpty({message: 'NOVA SENHA é obrigatória.'})
     @IsString({message: 'NOVA SENHA deve ser do tipo texto.'})
     newPassowrd: string;
 
+    @ApiProperty()
     @IsNotEmpty({message: 'CONFIRMAÇÃO DE SENHA é obrigatória.'})
     @IsString({message: 'CONFIRMAÇÃO DE SENHA deve ser do tipo texto.'})
-    ConfirmNewPassword: string;
+    confirmNewPassword: string;
 }
