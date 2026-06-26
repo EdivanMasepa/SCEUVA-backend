f05486181fc3f13daf9f693be1fa3365907cfde5
Edivan <edimasepa@gmail.com>
2026-06-26 20:15:56 -0300

    fix (user): correção de mensagens de retorno em endpoints

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -16,50 +16,89 @@ export class UserController {
 
   @Post()
   @ApiResponses([
-    { status: 201, description: 'Usuário cadastrado com sucesso.'},
-    { status: 400, description: 'Erro de validação, verifique os dados e tente novamente.'},
-    { status: 500, description: 'Erro interno. Verifique os dados e tente novamente.'}
+    {status: 201, description: 'Usuário cadastrado com sucesso.'},
+    {status: 400, description: 'E-mail já cadastrado..'},
+    {status: 400, description: 'Número de telefone já cadastrado..'},
+    {status: 400, description: 'As senhas não conferem.'},
+    {status: 400, description: 'CPF inválido.'},
+    {status: 400, description: 'CPF já cadastrado.'},
+    {status: 400, description: 'CNPJ inválido.'},
+    {status: 400, description: 'CNPJ já cadastrado.'},
+    {status: 400, description: 'Tipo de usuário inválido.'},
+    {status: 500, description: 'Erro interno. Verifique os dados e tente novamente.'}
   ])
   create(@Body() createUserDto: CreateUserDTO) {
     return this.userService.create(createUserDto);
+  }
+  
+  @Get(':id')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiResponses([
+    {status: 200, type: ListUserDTO},
+    {status: 400, description: 'Usuário não encontrado.'},
+    {status: 500, description: 'Erro interno. Verifique os dados e tente novamente.'}
+  ])
+  findOne(@Param('id') id: string) { console.log
+    return this.userService.findOne(+id);
   }
 
   @Get()
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
-  @ApiResponses([{status: 200, type: ListUserDTO}])
+  @ApiResponses([
+    {status: 200, type: [ListUserDTO]},
+    {status: 500, description: 'Erro interno. Verifique os dados e tente novamente.'}
+  ])
   findAll() {
     return this.userService.findAll();
-  }
-
-  @Get(':id')
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard('jwt'))
-  @ApiResponses([{status: 200, type: ListUserDTO}])
-  findOne(@Param('id') id: string) { console.log
-    return this.userService.findOne(+id);
   }
 
   @Patch()
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
-  @ApiResponses([{status: 200, description: 'Atualizado com sucesso.'}])
-  update(@Request() req, @Body() updateUserDto: UpdateUserDTO) {console.log(req.user.id)
+  @ApiResponses([
+    {status: 200, description: 'Atualizado com sucesso.'},
+    {status: 400, description: 'E-mail já cadastrado.'},
+    {status: 400, description: 'Número de telefone já cadastrado.'},
+    {status: 400, description: 'Usuário não possui cadastro de pessoa.'},
+    {status: 400, description: 'Usuário não possui cadastro de instituição.'},
+    {status: 401, description: 'Usuário não autorizado.'},
+    {status: 404, description: 'Usuário inválido.'},
+    {status: 500, description: 'Erro ao atualizar dados. Verifique os dados e tente novamente.'}
+  ])
+  update(@Request() req, @Body() updateUserDto: UpdateUserDTO) {
     return this.userService.update(req.user.id, updateUserDto);
+  }
+  
+  @Post('change-password')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiResponses([
+    {status: 200, description: 'Senha atualizada com sucesso.'},
+    {status: 400, description: 'Senha atual incorreta.'},
+    {status: 400, description: 'As senhas não conferem.'},
+    {status: 400, description: 'A nova senha não pode ser igual à senha atual.'},
+    {status: 401, description: 'Usuário não autorizado.'},
+    {status: 404, description: 'Usuário inválido.'},
+    {status: 500, description: 'Erro ao atualizar senha. Verifique os dados e tente novamente.'}
+  ])
+  changePassword(@Request() req, @Body() changePassword: ChangePasswordDTO) {
+    return this.userService.changePassword(req.user.id, changePassword);
   }
 
   @Delete()
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
-  @ApiResponses([{status: 200, description: 'Cadastro excluído com sucesso.'}])
+  @ApiResponses([
+    {status: 200, description: 'Cadastro excluído com sucesso.'},
+    {status: 400, description: 'Senha não informada.'},
+    {status: 400, description: 'Senha inválida.'},
+    {status: 401, description: 'Usuário não autorizado.'},
+    {status: 404, description: 'Usuário inválido.'},
+    {status: 500, description: 'Erro ao excluir conta. Tente novamente.'},
+  ])
   remove(@Request() req, @Body() password: RemoveAccountDTO) {
     return this.userService.remove(req.user.id, password.password);
-  }
-
-  @Post('change-password')
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard('jwt'))
-  changePassword(@Request() req, @Body() changePassword: ChangePasswordDTO) {
-    return this.userService.changePassword(req.user.id, changePassword);
   }
 }
